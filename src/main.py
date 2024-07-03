aa8a0b4cf041428a24436466c95cf828b24d399a
Jonathan Otto <jonootto@gmail.com>
2024-07-03 15:57:55 +1200

    pg

--- src/main.py
+++ src/main.py
@@ -4,12 +4,24 @@ from meshtastic import mesh_pb2, mqtt_pb2, portnums_pb2, telemetry_pb2
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import base64
-import random
-import sqlite3
+#import sqlite3
 from collections import deque
 from colorama import Fore, Back, Style
+import json
+import datetime
+import psycopg
+from dotenv import load_dotenv
 
+load_dotenv()
 # Default settings
+
+db_name = "meshtastic"
+db_host = "10.1.100.252"
+db_user = "postgres"
+db_pass = "example"
+db_port = "5432"
+db_connection_string = ("dbname=" + db_name + " host=" + db_host + " user=" + db_user + " password=" + db_pass + " port=" + db_port)
+
 MQTT_BROKER = "mqtt.meshtastic.org"
 MQTT_PORT = 1883
 MQTT_USERNAME = "meshdev"
@@ -23,12 +35,8 @@ padded_key = key.ljust(len(key) + ((4 - (len(key) % 4)) % 4), '=')
 replaced_key = padded_key.replace('-', '+').replace('_', '/')
 key = replaced_key
 
-#broadcast_id = 4294967295
-# Convert hex to int and remove '!'
-#node_number = int('abcd', 16)
-db = "node.db"
 
-message_ids = deque([],50)
+message_ids = deque([],200)
 
 def process_message(mp, text_payload, is_encrypted):
 
@@ -55,43 +63,43 @@ def decode_encrypted(message_packet):
         data = mesh_pb2.Data()
         data.ParseFromString(decrypted_bytes)
         message_packet.decoded.CopyFrom(data)
-        node_db(message_packet,db)
-
-        if message_packet.decoded.portnum == portnums_pb2.TEXT_MESSAGE_APP:
-            text_payload = message_packet.decoded.payload.decode("utf-8")
-            is_encrypted = True
-            process_message(message_packet, text_payload, is_encrypted)
-            print("TEXT_MESSAGE_APP")
-            print(f"{text_payload}")
-
-
-        elif message_packet.decoded.portnum == portnums_pb2.NODEINFO_APP:
+        info = False
+        pos = False
+        env = False
+        if message_packet.decoded.portnum == portnums_pb2.NODEINFO_APP:
             info = mesh_pb2.User()
             info.ParseFromString(message_packet.decoded.payload)
-            print("NODEINFO_APP")
+            #print("NODEINFO_APP")
 
-            print(info)
+            #print(info)
         elif message_packet.decoded.portnum == portnums_pb2.POSITION_APP:
             pos = mesh_pb2.Position()
             pos.ParseFromString(message_packet.decoded.payload)
-            print("POSITION_APP")
-            print(pos)
+            #print("POSITION_APP")
+            #print(pos)
 
         elif message_packet.decoded.portnum == portnums_pb2.TELEMETRY_APP:
             env = telemetry_pb2.Telemetry()
             env.ParseFromString(message_packet.decoded.payload)
-            print("TELEMETRY_APP")     
-            print(env)
+            #print(Fore.LIGHTGREEN_EX + "TELEMETRY_APP" + Style.RESET_ALL)     
+            #print(env)
+
+        #elif message_packet.decoded.portnum == portnums_pb2.TEXT_MESSAGE_APP:
+            #text_payload = message_packet.decoded.payload.decode("utf-8")
+            #is_encrypted = True
+            #process_message(message_packet, text_payload, is_encrypted)
+            #print("TEXT_MESSAGE_APP")
+            #print(f"{text_payload}")
+
     except Exception as e:
         print(f"Decryption failed: {str(e)}")
+    node_db(message_packet,info,pos,env)
 
 def on_connect(client, userdata, flags, rc, properties):
     if rc == 0:
         print(f"Connected to {MQTT_BROKER} on topic {channel}")
     else:
         print(f"Failed to connect to MQTT broker with result code {str(rc)}")
-
-
 
 
 def message_seen(message_packet):
@@ -117,76 +125,158 @@ def on_message(client, userdata, msg):
     
     if message_packet.HasField("encrypted") and not message_packet.HasField("decoded"):
         if not message_seen(message_packet):
-            print(Fore.CYAN + str(message_packet) + Style.RESET_ALL)
+            #print(Fore.CYAN + str(message_packet) + Style.RESET_ALL)
             decode_encrypted(message_packet)
         else:
             print(Fore.RED + "Skipping already seen message" + Style.RESET_ALL)
 
 
-
-
-def node_db(message_packet,filename):
+def node_db(message_packet,info,pos,env):
     sender = str(getattr(message_packet, "from"))
-    conn = sqlite3.connect(filename)
+    conn = psycopg.connect(db_connection_string)
     cursor = conn.cursor()
-    cursor.execute("SELECT id FROM nodes WHERE id = '" + sender + "'")
+    cursor.execute("SELECT id FROM nodes WHERE id=%s",(sender,))
     nodes = cursor.fetchall()
     if len(nodes) == 0:
         #New node
-        cursor.execute("INSERT INTO nodes (id) VALUES ('"+ sender + "')")
+        cursor.execute("INSERT INTO nodes (id) VALUES (%s)",(sender,))
         print("New node added to DB")
-        conn.commit()
+        
+    #add info
+    lastHeard = (getattr(message_packet, "rx_time"))
+    timestamp = datetime.datetime.fromtimestamp(lastHeard,datetime.UTC)
+    cursor.execute('UPDATE nodes SET LastHeard=%s WHERE id=%s', (timestamp, sender))
+
+    if info:
+        long_name = str(getattr(info, "long_name"))
+        short_name = str(getattr(info, "short_name"))
+        hw_model = str(getattr(info, "hw_model"))
+        role = str(getattr(info, "role"))
+        cursor.execute('UPDATE nodes SET long_name=%s, short_name=%s, hardware=%s, role=%s WHERE id = %s', (long_name, short_name, hw_model ,role , sender))
+
+    elif pos:
+        try:
+            lat = str(getattr(pos, "latitude_i")/10000000)
+            lon = str(getattr(pos, "longitude_i")/10000000)
+        except:
+            lat = None
+            lon = None
+        alt = str(getattr(pos, "altitude",None))
+        if int(alt) > 32000:
+            print("Impossible ALT")
+            alt = None
+        cursor.execute('UPDATE nodes SET latitude=%s, longitude=%s, altitude=%s WHERE id=%s', (lat, lon, alt, sender))   
+
+    elif env:
+        dev = getattr(env, "device_metrics")
+        telem = {
+            "battery_level": None,
+            "voltage": None,
+            "channel_utilization": None,
+            "air_util_tx": None
+        }
+        value = False
+        for metric in telem.keys():
+
+            newval = (getattr(dev, metric))
+            try:
+                nullcheck = int(newval*100)
+                if nullcheck == 0:
+                    output = None
+                else:
+                    output = str(round(newval,3))
+                    value = True
+            except:
+                output = None
+            telem.update({metric:output})
+        telem["id"] = sender
+        if value:
+            print(json.dumps(telem,indent=4))
+            cursor.execute('UPDATE nodes SET battery_level=%s, voltage=%s, channel_utilization=%s, air_util_tx=%s WHERE id = %s', (telem["battery_level"], telem["voltage"], telem["channel_utilization"], telem["air_util_tx"], telem["id"])) 
+            
+            #cursor.execute('INSERT INTO telemetry (node, timestamp, battery_level, voltage, channel_utilization, air_util_tx) VALUES (%s,%s,%s,%s,%s,%s)',(telem["id"],timestamp,))
+
+
+    conn.commit()
     conn.close()
     return
 
-def create_database(filename):
+def check_database():
     conn = None
     try:
-        conn = sqlite3.connect(filename)
-        print(sqlite3.sqlite_version)
-    except sqlite3.Error as e:
+        conn = psycopg.connect(db_connection_string)
+    except psycopg.Error as e:
         print(e)
+        exit()
     finally:
         if conn:
             conn.close()
 
-def setup_tables(filename):
-    sql_statements = [ 
-        """CREATE TABLE IF NOT EXISTS nodes (
-                id INTEGER PRIMARY KEY, 
-                long_name text,
-                short_name text,
-                hardware text,
-                latitude REAL,
-                longitude REAL,
-                altitude INTEGER,
-                battery INTEGER,
-                voltage REAL,
-                chutil REAL,
-                txutil REAL,
-                LastHeard INTEGER,
-                begin_date TEXT, 
-                end_date TEXT
-        );"""]
+def setup_tables():
+    statements = ["""CREATE TABLE IF NOT EXISTS nodes (
+                id BIGINT PRIMARY KEY, 
+                long_name VARCHAR(128),
+                short_name VARCHAR(8),
+                hardware VARCHAR(32),
+                latitude decimal,
+                longitude decimal,
+                altitude smallint,
+                battery_level smallint,
+                voltage decimal,
+                channel_utilization decimal,
+                air_util_tx decimal,
+                role VARCHAR(32),
+                LastHeard TIMESTAMPTZ
+                );""",
+                """CREATE TABLE IF NOT EXISTS telemetry (
+                id SERIAL PRIMARY KEY,
+                node BIGINT REFERENCES nodes(id),
+                timestamp TIMESTAMPTZ NOT NULL,
+                battery_level smallint,
+                voltage decimal,
+                channel_utilization decimal,
+                air_util_tx decimal
+                );"""
+                ]
     try:
-        with sqlite3.connect(filename) as conn:
-            cursor = conn.cursor()
-            for statement in sql_statements:
+        with psycopg.connect(db_connection_string) as conn:
+            for statement in statements:
+                cursor = conn.cursor()
                 cursor.execute(statement)
-            
-            conn.commit()
-    except sqlite3.Error as e:
+                conn.commit()
+    except psycopg.Error as e:
         print(e)
 
 
-
 def setup():
-    create_database(db)
-    setup_tables(db)
+    check_database()
+    setup_tables()
 
-setup()
+def loadDB():
+    statement = "SELECT * FROM nodes"# WHERE id = 2990211348"
+    with psycopg.connect(db_connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute(statement)
+        r = [dict((cursor.description[i][0], value) \
+            for i, value in enumerate(row)) for row in cursor.fetchall()]
+    node_info = {}
+
+    for item in r:
+        nextnode = {}
+        for data in item:
+            if data != "id":
+                nextnode.update({data:item[data]})
+        node_info.update({str(item["id"]):nextnode})
+    print(json.dumps(node_info["2990211348"],indent=4))
+    print("loaded db")
+    return node_info
+
+def publishMetrics():
+    node_info = loadDB(db)
 
 if __name__ == '__main__':
+    setup()
+
 
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_connect = on_connect
@@ -198,6 +288,10 @@ if __name__ == '__main__':
     subscribe_topic = f"{root_topic}{channel}/#"
 
     client.subscribe(subscribe_topic, 0)
-
+    x = 0
     while client.loop() == 0:
+        x += 1
+        if x == 10:
+            #publishMetrics()
+            x=0
         pass
