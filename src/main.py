6b1f0604ba10b0b734663c5ba075071a09ea5374
Jonathan Otto <jonootto@gmail.com>
2024-08-18 12:28:28 +1200

    nztime

--- src/main.py
+++ src/main.py
@@ -358,7 +358,7 @@ def checkOffline():
                 if email:
                     logging.warning('Max time exceeded for ID: %s %s, Last Heard (Hours): %s, Max Age: %s - emailing %s from %s', id, shortname, total_hours, max_hours, email, email_sender)
                     subject = 'Meshtastic node %s - %s offline' % (id, shortname)
-                    localtimestamp = timestamp.astimezone(ZoneInfo('localtime'))
+                    localtimestamp = timestamp.astimezone(ZoneInfo('Pacific/Auckland'))
                     body = 'Node %s - %s was last seen at %s, %s hours ago' % (id,shortname, localtimestamp, total_hours)
                     #send email
                     send_email(subject,body,email)
