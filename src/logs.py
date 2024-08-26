4673e1e28afd708c1e65413d394e0fc1b05c75c0
Jonathan Otto <jonootto@gmail.com>
2024-08-26 15:59:10 +1200

    logging

--- src/logs.py
+++ src/logs.py
@@ -1,5 +1,10 @@
 import logging
 from colorama import Fore, Style
+from config import testmode
+
 
 FORMAT = '%(levelname)s: %(asctime)s - %(message)s'
-logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt='%H:%M:%S')
+if testmode:
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt='%H:%M:%S')
+else:
+    logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt='%H:%M:%S')
